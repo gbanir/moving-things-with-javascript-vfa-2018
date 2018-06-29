f881656aabc81cdeadfeb6fb9b8c48640d94d9a3
Gaby Nair <gaby1113@gmail.com>
2018-06-29 20:18:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ dodger.style.left // "180px"
 dodger.style.bottom // "0px"
 dodger.style.bottom = '100px'
 var dodger = document.getElementById('dodger')
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.right = `${left + 1}px`
+  }
+}
